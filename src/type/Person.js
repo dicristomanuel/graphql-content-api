b05a4017674cbf11ccc616c42015e83b03cd681a
dicristomanuel <manuel.dicristo@raise.com>
2017-04-27 11:12:56 -0500

    [app] building type structure

--- src/type/Person.js
+++ src/type/Person.js
@@ -3,10 +3,14 @@ import {
   GraphQLObjectType,
 } from 'graphql';
 
+import { globalIdField } from 'graphql-relay';
+// fromGlobalId
+
 const Person = new GraphQLObjectType({
-  /* ... */
+  name: 'Person',
+  description: 'Represent Person',
   fields: () => ({
-    /* ... */
+    id: globalIdField('Person'),
     friends: {
       type: new GraphQLList(PersonType),
       resolve: person => person.friends.map(getPersonByURL),
